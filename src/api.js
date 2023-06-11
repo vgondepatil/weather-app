1b1b3d89a66cb0cad49436960d1be413bd3bf53a
Varad Gondepatil <vgondepatil@gmail.com>
2023-06-11 17:12:21 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c75f651558msh5fd1932cb916bfap124ad9jsn6a4ccb483deb",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8f8631bf85d3f711a7e4e77148c849f9"; // enter your key from openweather API
